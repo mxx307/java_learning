a0812832b8becd056dca86b700b1db4ea4cc458a
mxx307 <1123731866@qq.com>
2021-09-27 22:58:14 +0800

    更新~~
    Signed-off-by: mxx307 <1123731866@qq.com>

--- src/HelloWorld/HelloWorld.java
+++ src/HelloWorld/HelloWorld.java
@@ -3,6 +3,6 @@ package HelloWorld;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("HelloWorld");
-        System.out.println("Hello");
+        System.out.println("Hello!");
     }
 }
