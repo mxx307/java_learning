7fd0f97dbce7db87eac437d1df9ec47c53c49652
mxx307 <1123731866@qq.com>
2021-10-08 14:10:39 +0800

    更新~~
    Signed-off-by: mxx307 <1123731866@qq.com>

--- src/test/test.java
+++ src/test/test.java
@@ -2,6 +2,6 @@ package test;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("this is just a test.");
+        System.out.println("this is just a test.wuhu!");
     }
 }
